9a917d7a9809930c424f7d9ca207747ade3a3e83
belal mohamed <belalmohamed3690@gmail.com>
2026-02-25 22:18:47 +0200

    now working

--- portsc.py
+++ portsc.py
@@ -1,12 +1,15 @@
 import sys,socket
 
-ip = sys.argv[1]
+ip =socket.gethostbyname(sys.argv[1])
 ports = sys.argv[2]
 
-request = socket.socket(socket.Af_INET,socket.SOCK_STREAM)
+request = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 
-for x in range(ports):
-    if request.connect_ex(f"{ip}",x):
+for x in range(int(ports)):
+
+    request = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    request.settimeout(0.1)
+    if not request.connect_ex((f"{ip}",x)):
         print(f"port {x} is open")
     else:
         continue
